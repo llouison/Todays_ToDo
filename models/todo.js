5315dfed8ea753035cc511b77b8f2693b3aa77bf
lisa louison <lisa.louison@gmail.com>
2017-04-28 23:48:17 -0400

    created todo model

---
+++ models/todo.js
@@ -0,0 +1,54 @@
+// importing our pgpromise
+const db = require('../db/config');
+
+// setting up our Todo model object
+const Todo = {};
+
+// creating the findall method
+Todo.findAll = () => {
+    return db.query('SELECT * FROM todo ORDER BY ASC');
+};
+
+// creating the findbyid method
+Todo.findById = id => {
+    return db.oneOrNone('SELECT * FROM todo WHERE id = $1', [id]);
+};
+
+// creating the add method
+Todo.create = todo => {
+    return db.one(
+        `
+        INSERT INTO todo
+        (title, description, category, status)
+        VALUES ($1, $2, $3) RETURNING *
+        `,
+        [todo.title, todo.description, todo.category, todo.status]
+    );
+};
+
+// creating the update method
+Todo.update = (quote, id) => {
+    return db.none(
+        `
+        UPDATE todo SET
+        title = $1,
+        description = $2,
+        category = $3,
+        status = $4
+        `,
+       [todo.title, todo.description, todo.category, todo.status] 
+    );
+};
+
+// creating the delete method
+Todo.destroy = id => {
+    return db.none(
+        `DELETE FROM todo
+        WHERE id = $1
+        `,
+        [id]
+    );
+};
+
+// exporting the todo model
+module.exports = Todo;
