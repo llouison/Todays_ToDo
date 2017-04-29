c2bdf9b3ecd9d848ad4b722fe92b73e2a675c888
lisa louison <lisa.louison@gmail.com>
2017-04-28 22:42:42 -0400

    updated config.js

--- db/config.js
+++ db/config.js
@@ -2,19 +2,22 @@
 const pgp = require('pg-promise')();
 
 // setting a variable for our instance of pg-promise
+let db;
 
+// creating if statements to determine the instance of pgp to use
 if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
     db = pgp({
-        database: '',
+        database: 'todo_development',
         port: 5432,
         host: 'localhost'
     });
 } else if (process.env.NODE_ENV === 'production') {
     db = pgp({
-        database: '',
+        database: 'todo_production',
         port: 5432,
         host: 'localhost'
     });
 }
 
+// exporting our pg-promise
 module.exports = db;
